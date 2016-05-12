17d80f6a58c6a1905ab80ffbf8ea56135fabe734
Shane Henning <shanerhenning@gmail.com>
2016-05-12 16:21:12 -0700

    integrated handlebars

--- js/project.js
+++ js/project.js
@@ -1,50 +1,21 @@
-//Hamburger Menu Animation
-var container = document.getElementById('container');
-var menu = document.getElementById('menu');
-menu.addEventListener('click', move);
-
-function move(){
-  container.className += 'move';
-  menu.removeEventListener('click', move);
-  menu.addEventListener('click', remove);
-}
-
-function remove(){
-  menu.removeEventListener('click', remove);
-  menu.addEventListener('click', move);
-  container.className = '';
-}
-
-//----------------------------------------------------------------------------
 var projects = [];
 
 function Project(details){
-  this.projectName = details.projectName;
-  this.projectDate = details.projectDate;
-  this.image = details.image;
-  this.link = details.link;
-  this.category = details.category;
-  this.description = details.description;
-
-  // for(key in details) this[key] = details[key];
+  for(key in details) this[key] = details[key];
 }
 
 Project.prototype.printToPage = function(){
-  var $newProject = $('article.project-template').clone();
-  $newProject.find('h2').html(this.projectName);
-  $newProject.find('h2 > a').attr('href', this.link);
-  $newProject.find('h5').html(this.projectDate);
-  $newProject.find('img').attr('src', this.image);
-  $newProject.attr('data-category', this.category);
-  $newProject.find('a:first').text(this.link).attr('href', this.link);
-  $newProject.find('p').html(this.description);
-
-  $newProject.find('h5').html('Produced ' + (Math.round((new Date() - new Date(this.projectDate)) / 60 / 60 / 24 / 1000)) + ' days ago.');
-
-  $newProject.removeClass('project-template');
-  return $newProject;
+  var $handlebarsSource = $('#new-project-template').html();
+  var template = Handlebars.compile($handlebarsSource);
+  // $('article').find('h5').html('Produced ' + (Math.round((new Date() - new Date(this.projectDate)) / 60 / 60 / 24 / 1000)) + ' days ago.');
+  return template(this);
 };
 
+Project.prototype.populateCategory = function(){
+  var $handlebarsSource = $('#select-template').html();
+  var template = Handlebars.compile($handlebarsSource);
+  return template(this);
+};
 
 allProjects.sort(function(a,b){
   return (new Date(b.projectDate) - (new Date(a.projectDate)));
@@ -55,9 +26,9 @@ allProjects.forEach(function(pushProject){
 });
 
 projects.forEach(function(appendProject){
-  $('.home').append(appendProject.printToPage());
+  $('.select-option').append(appendProject.populateCategory());
 });
 
-(function removeTemplate(){
-  $('article.project-template').remove();
-})();
+projects.forEach(function(appendProject){
+  $('.project-location').append(appendProject.printToPage());
+});
