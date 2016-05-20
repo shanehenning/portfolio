56a5941746960a33e8acb1d0e039282bd14c6ed0
Shane Henning <shanerhenning@gmail.com>
2016-05-20 14:51:00 -0700

    fixed weird nav bar push

--- js/projectSelector.js
+++ js/projectSelector.js
@@ -1,53 +1,57 @@
-var projectSelector = {};
+(function(module){
 
-projectSelector.handleCategory = function(){
-  $('.select-option').on('change', function(){
-    var $projectsListed = $('article');
-    var $selection = $(this).val();
-    if($selection){
-      $projectsListed.hide();
-      $projectsListed.each(function(){
-        if($(this).attr('data-category') === $selection){
+  var projectSelector = {};
+
+  projectSelector.handleCategory = function(){
+    $('.select-option').on('change', function(){
+      var $projectsListed = $('article');
+      var $selection = $(this).val();
+      if($selection){
+        $projectsListed.hide();
+        $projectsListed.each(function(){
+          if($(this).attr('data-category') === $selection){
+            $(this).show();
+          }
+        });
+      } else{
+        $projectsListed.show();
+      }
+    });
+  };
+
+  projectSelector.showMore = function(){
+    $('.home p:nth-of-type(n+3)').hide();
+    $('article').on('click', 'a', function(){
+      $(this).parent().find('*').show();
+      $(this).hide();
+    });
+  };
+
+  projectSelector.oneTab = function(){
+    $('.navi').on('click', 'a', function(e){
+      e.preventDefault();
+      $('section').hide();
+      var link = $(this).attr('href');
+      $('section').each(function(){
+        if(link === $(this).attr('class')){
           $(this).show();
         }
       });
-    } else{
-      $projectsListed.show();
-    }
-  });
-};
-
-projectSelector.showMore = function(){
-  $('.home p:nth-of-type(n+3)').hide();
-  $('article').on('click', 'a', function(){
-    $(this).parent().find('*').show();
-    $(this).hide();
-  });
-};
-
-projectSelector.oneTab = function(){
-  $('.navi').on('click', 'a', function(e){
-    e.preventDefault();
-    $('section').hide();
-    var link = $(this).attr('href');
-    $('section').each(function(){
-      if(link === $(this).attr('class')){
-        $(this).show();
-      }
     });
-  });
-  $('.navi a:first').click();
-};
+    $('.navi a:first').click();
+  };
 
-projectSelector.printIndexPage = function(){
-  Project.all.forEach(function(appendProject){
-    $('.select-option').append(appendProject.populateCategory());
-    $('.project-location').append(appendProject.printToPage());
-  });
-};
+  projectSelector.printIndexPage = function(){
+    Project.all.forEach(function(appendProject){
+      $('.select-option').append(appendProject.populateCategory());
+      $('.project-location').append(appendProject.printToPage());
+    });
+  };
 
-$(document).ready(function(){
-  projectSelector.handleCategory();
-  projectSelector.showMore();
-  projectSelector.oneTab();
-});
+  $(document).ready(function(){
+    projectSelector.handleCategory();
+    projectSelector.showMore();
+    projectSelector.oneTab();
+  });
+  module.projectSelector = projectSelector;
+})(window);
